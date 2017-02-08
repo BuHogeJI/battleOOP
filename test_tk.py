bf17672d35d3a178d7177131e5bbf85e09107e3d
BuHogeJI <ddv@iptel.by>
2017-02-08 21:30:19 +0300

    Add files via upload

--- test_tk.py
+++ test_tk.py
@@ -1,32 +1,37 @@
-from tkinter import *
 
-class Sea():
+
+class Sea:
+	
+	def makeSea(self):
+		self.sea = Canvas(root, width = 1000, height = 500, bg = 'black', cursor = 'plus')
+		self.sea.bind('<Button-1>', self.move)
 
 	def makeBoard(self):
-		self.board = Frame(root)
-		for r in range(10):
-			for c in range(10):
-				self.button = Button(self.board, bg = 'white', width = 6, height = 2)
-				self.button.bind('<Button-1>', self.miss)
-				self.button.grid(row  = r, column = c)
-		self.board.grid(row = 0, column = 0)
-		f = Frame(root, width = 100).grid(row = 0, column = 1)
+		i = 30
+		x = 0
+		y = 30
+		j = 1
+		while j < 10:
+			x = 0
+			while x < 300:
+				self.sea.create_rectangle(x, y, x + 30, y + 30, fill = 'white', tag = '{}'.format(string.ascii_letters[j], j))
+				x += 30
+			y += i
+			j += 0
 
-	def makeEnemyBoard(self):
-		self.enemy_board = Frame(root)
-		for r in range(10):
-			for c in range(10):
-				self.button = Button(self.enemy_board, bg = 'white', width = 6, height = 2)
-				self.button.bind('<Button-1>', self.miss)
-				self.button.grid(row  = r, column = c)
-		self.enemy_board.grid(row = 0, column = 2)
+	def move(self, event):
+		self.sea.itemconfig('sea', fill = 'pink')
+		
 
-	def miss(self, event):
-		self.button['bg'] = 'lightblue'
+	
 
 if __name__ == '__main__':
+	from tkinter import *
+	import string
+
 	root = Tk()
-	sea = Sea()
-	sea.makeBoard()
-	sea.makeEnemyBoard()
+	canv = Sea()
+	canv.makeSea()
+	canv.makeBoard()
+	canv.sea.pack()
 	root.mainloop()
